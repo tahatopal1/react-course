09e58a529d0e15aa733b6521f534a58ab2b07360
tahatopal <taha.topal@scalefocus.com>
2025-01-02 19:49:37 +0300

    children props

---
+++ u3_ReactEssentials/s48_StoringStylesInFiles/react-project/src/App.jsx
@@ -0,0 +1,34 @@
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
