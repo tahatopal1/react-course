b67559df91db011cff7df0900619d08178918c17
tahatopal <taha.topal@scalefocus.com>
2025-01-07 18:52:48 +0300

    Splitting CSS

---
+++ u5_PracticeProject/starting-project/src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid =
+    userInput.initialInvestment >= 1 &&
+    userInput.annualInvestment >= 1 &&
+    userInput.expectedReturn >= 1 &&
+    userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {inputIsValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please enter a value greater than zero.</p>
+      )}
+    </>
+  );
+}
+
+export default App;
