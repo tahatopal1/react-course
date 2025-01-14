e235835bf23f421d20b86886a1f8ce467b7b8f1d
tahatopal <taha.topal.7@gmail.com>
2025-01-14 03:48:43 +0300

    Portals

---
+++ u8_RefsAndPortals/s141_ExposingComponentAPIImperativeHandle/starting-project/src/components/ResultModal.jsx
@@ -0,0 +1,29 @@
+import { useImperativeHandle } from "react";
+import { useRef } from "react";
+
+export default function ResultModal({ ref, result, targetTime }) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return (
+    <dialog ref={dialog} className="result-modal">
+      <h2>You {result}</h2>
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with <strong>X seconds left.</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+}
