6df5c888b82e9fe0be0083e576a2d7d723240dd5
tahatopal <taha.topal.7@gmail.com>
2025-03-06 06:47:44 +0300

    Animating

---
+++ u23_AuthenticatonOnReactApps/s397_AddingAutomaticLogout/starting-project/frontend/src/pages/Root.js
@@ -0,0 +1,44 @@
+import {
+  Outlet,
+  useNavigation,
+  useLoaderData,
+  useSubmit,
+} from "react-router-dom";
+
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
+
+function RootLayout() {
+  const token = useLoaderData();
+  const submit = useSubmit();
+  // const navigation = useNavigation();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { action: "/logout", method: "post" });
+    }, tokenDuration);
+  }, [token, submit]);
+
+  return (
+    <>
+      <MainNavigation />
+      <main>
+        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+        <Outlet />
+      </main>
+    </>
+  );
+}
+
+export default RootLayout;
